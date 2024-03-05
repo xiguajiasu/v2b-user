e383740c16c2a847fad37ec8a8a89a7699699ea2
xiguajiasu <121231289+xiguajiasu@users.noreply.github.com>
2024-03-05 23:20:53 +0800

    Update env.js

--- env.js
+++ env.js
@@ -1,14 +1,14 @@
 window.settings = {
-  title: 'Tgfish飞鱼',
-  description: '与您一起遨游国际网络海洋',
-  host: 'https://tfapi.cbugcloud.com',
+  title: '西瓜加速',
+  description: '快到没朋友',
+  host: 'https://jjj.xiguajiasu.store',
   theme: {
     sidebar: 'light',
     header: 'dark',
-    color: 'default'
+    color: 'green'
   },
   
-  background_url: 'https://f.ipip.dog/img/bg5.png',
+  background_url: 'https://zzz.xiguajiasu.store/bj.jpp',
   // crisp
  // crisp_id: '23a4c4a3-9cc7-4405-ad1f-d966eabb4c04',
   i18n: [
